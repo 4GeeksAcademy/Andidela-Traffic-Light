792953b62ece44c105a2974f8330051170701e24
Andidela <andidela83@gmail.com>
2024-08-29 03:08:01 +0000

    Proyecto Finalizado

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,15 @@
 //import react into the bundle
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 
-// include your styles into the webpack bundle
+//include bootstrap npm library into the bundle
+import "bootstrap";
+
+//include your index.scss file into the bundle
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import { TrafficLight } from "/workspaces/Andidela-Traffic-Light/src/js/TrafficLight.js";
 
 //render your react application
-ReactDOM.createRoot(document.getElementById('app')).render(<Home/>);
-
+ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
